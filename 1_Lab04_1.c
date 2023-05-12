88c2baee0221343d6ef198da9257cfea9091b072
soyoung456 <147dudth@naver.com>
2023-05-12 10:04:58 +0900

    1_Lab04

---
+++ 1_Lab04_1.c
@@ -0,0 +1,10 @@
+// Baekjoon Online Judge # 2789 ±¸±¸´Ü
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i < 10; i++)
+        printf("%d * %d = %d\n", n, i, n * i);
+    return 0;
+}
