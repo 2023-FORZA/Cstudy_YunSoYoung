00d77b3cc60c5b808d5ba0eb1a84715e8f5b789b
soyoung456 <147dudth@naver.com>
2023-05-14 03:47:34 +0900

    1_Lab04

---
+++ 1_Lab04_2.c
@@ -0,0 +1,15 @@
+// Baekjoon Online Judge # 8393 гу
+#include <stdio.h>
+
+int main()
+{
+    int n, dap = 0;
+    scanf("%d", &n);
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        dap += i;
+    }
+    printf("%d\n", dap);
+    return 0;
+}
