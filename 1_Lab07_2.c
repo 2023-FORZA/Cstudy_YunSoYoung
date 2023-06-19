80c84250128d222adc8c5fad7c6080b0f21d0456
soyoung456 <147dudth@naver.com>
2023-06-20 02:16:41 +0900

    1_Lab07

---
+++ 1_Lab07_2.c
@@ -0,0 +1,24 @@
+// Baekjoon Online Judge # 10809
+
+#include <stdio.h>
+
+int main(void)
+{
+    char S[101];
+    scanf("%s", S);
+    for (int ascii = 97; ascii < 123; ascii++)
+    {
+        int count = 0;
+        for (int i = 0; S[i] != NULL; i++)
+        {
+            if (S[i] == ascii)
+            {
+                printf("%d ", i);
+                count++;
+                break;
+            }
+        }
+        if (count == 0)
+            printf("-1");
+    }
+}
